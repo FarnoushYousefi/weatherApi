a67bfa8685b98c83d913e0677a975b5deadae61b
Farnoush <farnoush.yousefikejani@uwaterloo.ca>
2021-06-06 20:48:49 -0400

    add a new click listener to each previous search button and don't run the error alert when the user clicks one of these buttons. Note: 'getUserRepos' should really be renamed and if it was pulled from another source, it should be refactored so that it cleanly fits with this application. Users aren't searching for repositories so the app shouldn't throw an error with this message

--- js/script.js
+++ js/script.js
@@ -6,6 +6,26 @@ var tempEl = document.querySelector('.temp');
 var forcastEl = document.querySelector('.forcast');
 var userFormEl = document.querySelector('#formSubmit');
 var task = [];
+var task = JSON.parse(localStorage.getItem('cities'));
+console.log(task);
+// function renderHistory() {
+//   $('#weatherButton').empty();
+//   if (localStorage.getItem('cities')) {
+//     task = JSON.parse(localStorage.getItem('cities'));
+//     for (var i = 0; i < task.length; i++) {
+//       $('#weatherButton').append(
+//         '<button class="btn btn-info btn-lg mt-2 mb-2 w-1oo p-2 search">' +
+//           task[i] +
+//           '</button>'
+//       );
+//     }
+//   }
+//   $('.serach').click(function () {
+//     searchWeather($(this).text());
+//   });
+// }
+
+//https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
 function renderHistory() {
   $('#weatherButton').empty();
   if (localStorage.getItem('cities')) {
@@ -25,6 +45,7 @@ function renderHistory() {
 
 //  $('.search').on('click', searchWeather());
 function searchWeather(cityText) {
+  var newObj = {};
   var apiUrl =
     'https://api.openweathermap.org/data/2.5/forecast?q=' +
     cityText +
@@ -33,13 +54,50 @@ function searchWeather(cityText) {
   // make a get request to url
   fetch(apiUrl)
     .then(function (response) {
+      // console.log(response);
       // request was successful
       if (response.ok) {
-        response.json().then(function (data) {
-          //   console.log(data);
+        response
+          .json()
+          .then(function (data) {
+            console.log(data);
+            newObj.cityName = data.city.name;
+            newObj.cityName = data.city.name;
+            newObj.temperature = data.list[0].main.temp;
+            newObj.description = data.list[0].weather[0].description;
+            newObj.date = data.list[0].dt_txt;
+            newObj.icon = data.list[0].weather[0].icon;
+            newObj.humidity = data.list[0].main.humidity;
+            newObj.speed = data.list[0].wind.speed;
 
-          display(data);
-        });
+            var lat = data.city.coord.lat;
+            var lon = data.city.coord.lon;
+            console.log('lat', lat);
+            console.log('lon', lon);
+            // display(newObj);
+            return fetch(
+              'https://api.openweathermap.org/data/2.5/onecall?lat=' +
+                lat +
+                '&lon=' +
+                lon +
+                '&appid=08dff11d3547ffd95ff0b6c1c1070466&timezone_offset=1'
+            );
+            // https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
+          })
+          .then(function (response) {
+            return response.json();
+          })
+          .then(function (data) {
+            console.log(data);
+            if (data.length === 0) {
+              console.log('there is no data');
+            } else {
+              var uv = data.current.uvi;
+              newObj.fiveday = data.daily;
+              newObj.uv = uv;
+              display(newObj);
+            }
+          });
       }
     })
     .catch(function (error) {
@@ -49,7 +107,7 @@ function searchWeather(cityText) {
 }
 
 var getUserRepos = function (user) {
-  // format the github api url
+  //format the github api url
   var apiUrl =
     'https://api.openweathermap.org/data/2.5/forecast?q=' +
     inputValue.value +
@@ -94,46 +152,65 @@ var display = function (res) {
   $('.display').html('');
   $('.forcast').html('');
   $('.display').append(`<div id=firstDisplay></div>`);
-  var nameValue = res.city.name;
-  var tempValue = res.list[0].main.temp;
-  var descValue = res.list[0].weather[0].description;
-  var dateValue = res.list[0].dt_txt;
-  var icon = res.list[0].weather[0].icon;
+  var nameValue = res.cityName;
+  var tempValue = res.temperature;
+  var descValue = res.description;
+  var dateValue = res.fiveday[0].dt * 1000;
+  var dateObj = new Date(dateValue);
+  var finalDate = dateObj.toLocaleString();
+
+  var icon = res.icon;
+  var humindtity = res.humidity;
+  var uv = res.uv;
+  var speed = res.speed;
+  $('.display').append(`<p id =uv>${uv}</p>`);
   $('#firstDisplay').append(`<p id=cityName>${nameValue}</p>`);
-  $('#firstDisplay').append(`<p id=tempValue>${tempValue} F</p>`);
+  $('#firstDisplay').append(
+    `<p id=tempValue>Temperature: '${tempValue} ˚F</p>`
+  );
   $('#firstDisplay').append(`<p id=descValue>${descValue}</p>`);
-  $('#firstDisplay').append(`<p id=dateValue>${dateValue}</p>`);
+  $('#firstDisplay').append(`<p id=dateValue>${finalDate}</p>`);
   $('#firstDisplay').append(
     `<img src="http://openweathermap.org/img/wn/${icon}.png"></img>`
   );
+  $('#firstDisplay').append(`<p id=dateValue>${humindtity} Humidity</p>`);
+  $('#firstDisplay').append(`<p id=dateValue>${speed} Speed</p>`);
   $('<div>').addClass('container');
-  for (var i = 0, j = 0; i < 5; i++) {
+  for (var i = 0; i < 5; i++) {
+    var dateValue = res.fiveday[i].dt * 1000;
+    var dateObj = new Date(dateValue);
+    var finalDate = dateObj.toLocaleString();
     $('.forcast').append(`<div class=col id=day` + i + `></div>`);
 
     $('#day' + i).append(`<p id=forcastDate` + i + `></p>`);
 
     // var dateValue = res.list[0].dt_text;
-    $('#forcastDate' + i).text(res.list[j].dt_txt);
+    $('#forcastDate' + i).text(finalDate);
     $('#day' + i).append('<p id=forcastTemp' + i + '></p>');
-    $('#forcastTemp' + i).text(res.list[j].main.temp + 'F');
+    $('#forcastTemp' + i).text('Temperature:' + res.fiveday[i].temp.day + '˚F');
     $('#day' + i).append(
-      `<img src="http://openweathermap.org/img/wn/${res.list[j].weather[0].icon}.png"></img>`
+      `<img id='icon' src="http://openweathermap.org/img/wn/${res.fiveday[i].weather[0].icon}.png"></img>`
     );
-    j = j + 8;
+    $('#day' + i).append(
+      ` <p id=dateValue> humindtity${res.fiveday[i].humidity} % </p>`
+    );
+    $('#day' + i).append(
+      ` <p id=dateValue>speed${res.fiveday[i].weather[0].wind_speed} miles/hour</p>`
+    );
   }
   if (task.indexOf(nameValue) == -1) {
     task.push(nameValue);
     localStorage.setItem('cities', JSON.stringify(task));
-    $('#weatherButton').append(
-      '<button class="btn btn-info btn-lg mt-2 mb-2 w-100 p-2" onClick="searchWeather()"> ' +
-        nameValue +
-        '</button>'
-    );
+    // $('#weatherButton').append(
+    //   '<button class="btn btn-info btn-lg mt-2 mb-2 w-100 p-2" onClick="searchWeather()"> ' +
+    //     nameValue +
+    //     '</button>'
+    // );
   }
 
-  // renderHistory();
+  renderHistory();
 };
 
 userFormEl.addEventListener('submit', formSubmitHandler);
-
+//page the load
 renderHistory();
